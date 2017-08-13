48f37435e1e29e22cb15afad3206de1067fc8ced
CharlieHuang <notwist123@hotmail.com>
2017-08-13 15:02:31 +0800

    add Dubstep.cs and DubstepTest.cs

---
+++ Kata/Dubstep.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class Dubstep
+    {
+        public string SongDecoder(string input)
+        {
+            input = trimStartAndEnd(input);
+            input = replaceSpace(input);
+            return input;
+        }
+
+        private string replaceSpace(string input)
+        {
+            if (input.Contains("WUB")) input = replaceSpace(input.Replace("WUB", " "));
+            if (input.Contains("  ")) input = replaceSpace(input.Replace("  ", " "));
+            return input;
+        }
+
+        private string trimStartAndEnd(string input)
+        {
+            if (input.StartsWith("WUB")) input = trimStartAndEnd(input.Substring(3));
+            if (input.EndsWith("WUB")) input = trimStartAndEnd(input.Substring(0, input.Length - 3));
+
+            return input;
+        }
+    }
+}
---
+++ KataUnitTest/DubstepTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using Kata;
+
+namespace KataUnitTest
+{
+    [TestFixture]
+    public class DubstepTest
+    { 
+        [Test]
+        [TestCase("ABC", "WUBWUBABCWUB")]
+        [TestCase("R L", "RWUBWUBWUBLWUB")]
+        public void BasicTests(string excepted, string inputStirng)
+        {
+            var kata = new Dubstep();
+            var actual = kata.SongDecoder(inputStirng);
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
