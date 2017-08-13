2a9f174446ff7de1d55085a21f30bc00894e2658
CharlieHuang <notwist123@hotmail.com>
2017-08-13 14:27:58 +0800

    add BuildTower.cs and BuildTowerTest.cs

---
+++ Kata/BuildTower.cs
@@ -0,0 +1,35 @@
+﻿namespace Kata
+{
+    public class BuildTower
+    {
+        public string[] TowerBuilder(int nFloors)
+        {
+            string[] towerList = new string[nFloors];
+
+            for (int i = nFloors; i > 0; i--)
+            {
+                towerList[nFloors - i] = getSpace(i) + getStar(i,nFloors) + getSpace(i);
+            }
+
+            return towerList;
+        }
+
+        private string getStar(int number, int floor)
+        {
+            string result = "*";
+
+            for (int i = floor; i > number; i--) result = result + "**";
+
+            return result;
+        }
+
+        private string getSpace(int number)
+        {
+            string result = "";
+
+            for (int i = number; i > 1; i--) result = result + " ";
+
+            return result;
+        }
+    }
+}
---
+++ KataUnitTest/BuildTowerTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using Kata;
+
+namespace KataUnitTest
+{
+    [TestFixture]
+    public class BuildTowerTest
+    {
+        [Test]
+        [TestCase(new[] { "*" }, 1)]
+        [TestCase(new[] { " * ", "***" }, 2)]
+        [TestCase(new[] { "  *  ", " *** ", "*****" }, 3)]
+        public void BasicTests(string[] outputStrings, int inputNum)
+        {
+            var kata = new BuildTower();
+
+            var actual = string.Join(",", kata.TowerBuilder(inputNum));
+            var excepted = string.Join(",", outputStrings);
+            
+            Assert.AreEqual(excepted,actual);
+        }
+    }
+}
