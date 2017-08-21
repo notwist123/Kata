0f2e6f4f07ebb8af3ab0665f5183ce2f4e3c8ee6
CharlieHuang <notwist123@hotmail.com>
2017-08-21 10:52:48 +0800

    add CountingDuplicates.cs and CountingDuplicatesTest.cs

---
+++ Kata/CountingDuplicates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class CountingDuplicates
+    {
+        public int DuplicateCount(string str)
+        {
+            if (!str.Equals(""))
+            {
+                return str.ToLower().Substring(1).Contains(str.ToLower().Substring(0, 1))
+                    ? 1 + DuplicateCount(str.ToLower().Replace(str.ToLower().Substring(0, 1), ""))
+                    : DuplicateCount(str.ToLower().Replace(str.ToLower().Substring(0, 1), ""));
+            }
+            return 0;
+        }
+    }
+}
---
+++ KataUnitTest/CountingDuplicatesTest.cs
@@ -0,0 +1,22 @@
+﻿using Kata;
+using NUnit.Framework;
+
+namespace KataUnitTest
+{
+    public class CountingDuplicatesTest
+    {
+        [Test]
+        [TestCase(0, "")]
+        [TestCase(0, "abcde")]
+        [TestCase(2, "aabbcde")]
+        [TestCase(2, "aabBcde")]
+        [TestCase(1, "Indivisibility")]
+        [TestCase(2, "Indivisibilities")]
+        public void BasicTests(int excepted, string inputStirng)
+        {
+            var kata = new CountingDuplicates();
+            var actual = kata.DuplicateCount(inputStirng);
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
