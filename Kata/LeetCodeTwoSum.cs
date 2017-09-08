6ac3d730334e647e644ef61f02b391e79716a7e6
CharlieHuang <notwist123@hotmail.com>
2017-09-09 02:02:23 +0800

    add LeetCodeTwoSum

---
+++ Kata/LeetCodeTwoSum.cs
@@ -0,0 +1,17 @@
+﻿namespace Kata
+{
+    public class LeetCodeTwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j]) == target) return new[] { i, j };
+                }
+            }
+            return new[] { -1 };
+        }
+    }
+}
---
+++ KataUnitTest/LeetCodeTwoSumTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Kata;
+
+namespace KataUnitTest
+{
+    [TestFixture]
+    class LeetCodeTwoSumTest
+    {
+        [Test]
+        [TestCase(new[] { 0, 1 }, new[] { 2, 7, 11, 15 }, 9)]
+        [TestCase(new[] { 1, 2 }, new[] { 2, 7, 11, 15 }, 18)]
+        [TestCase(new[] { 1, 2 }, new[] { 3, 2, 4 }, 6)]
+        public void BasicTests(int[] outputArray, int[] inputArray, int countingNumber)
+        {
+            var kata = new LeetCodeTwoSum();
+
+            var actual = kata.TwoSum(inputArray, countingNumber);
+            var excepted = outputArray;
+
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
