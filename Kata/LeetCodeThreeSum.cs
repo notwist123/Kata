23bedfd441d88ef996ab8d7e145daa8f16b8c8bd
CharlieHuang <notwist123@hotmail.com>
2017-09-20 23:40:14 +0800

    add LeetCodeThreeSum

---
+++ Kata/LeetCodeThreeSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class LeetCodeThreeSum
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums);
+            List<IList<int>> result = new List<IList<int>>();
+
+            for (var i = 0; i < nums.Length -2; i++)
+            {
+                int firstNum = nums[i];
+
+                for (var j = i + 1; j < nums.Length - 1; j++)
+                {
+                    int nextNum = nums[j];
+                    int startSearchIndex = j + 1;
+                    int searchLength = nums.Length - startSearchIndex;
+                    int searchValue = (firstNum + nextNum) * -1;
+                    int thirdNumIndex = Array.BinarySearch(nums, startSearchIndex, searchLength, searchValue);
+
+                    if (thirdNumIndex < 0) continue;
+
+                    int[] addNum = {firstNum, nextNum, nums[thirdNumIndex]};
+                    result.Add(addNum);
+
+                    while (j < nums.Length - 1 && nums[j] == nums[j + 1]) j++;
+                }
+                while (i < nums.Length - 1 && nums[i] == nums[i + 1]) i++;
+            }
+            return result;
+        }
+    }
+}
---
+++ KataUnitTest/LeetCodeThreeSumTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Kata;
+
+namespace KataUnitTest
+{
+    [TestFixture]
+    class LeetCodeThreeSumTest
+    {
+        [Test]
+        [TestCase(new[] { -1, 0, 1, 2, -1, -4 , -1 },  new[] { -1, -1, 2 },new[] { -1, 0, 1 })]
+        public void BasicTests(int[] inputArray, params object[] outputArray)
+        {
+            var kata = new LeetCodeThreeSum();
+
+            var actual = kata.ThreeSum(inputArray);
+            var excepted = new List<IList<int>>();
+
+            foreach (int[] n in outputArray)
+            {
+                excepted.Add(n.ToList());
+            }
+
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
