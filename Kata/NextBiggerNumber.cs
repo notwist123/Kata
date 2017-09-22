df6d243fc8c541cc27678ae5bf688a4306c0525e
CharlieHuang <notwist123@hotmail.com>
2017-09-22 11:23:05 +0800

    add NextBiggerNumberTest

---
+++ Kata/NextBiggerNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class NextBiggerNumber
+    {
+        private List<long> numberList = new List<long>(); 
+        public long NextBigger(long n)
+        {
+            findNumbers("",n.ToString());
+            numberList.Sort();
+
+            return checkNumber(n.ToString())? numberList[numberList.FindIndex(x => x ==n) + 1] : -1;
+        } 
+
+        private void findNumbers(string combine, string number)
+        {
+            if (number.Length == 0 && !numberList.Contains(Convert.ToInt64(combine))) 
+                numberList.Add(Convert.ToInt64(combine));              
+            else
+            {
+                for (int i = 0; i < number.Length; i++)
+                {
+                    findNumbers(combine + number[i], number.Substring(0,i) + number.Substring(i+1));
+                }
+            }
+        }
+
+        private bool checkNumber(string number)
+        {
+            if (number.Length == 1) return false;
+            if (number.GroupBy(x => x).Count() == 1) return false;
+            if (Convert.ToInt64(number) == numberList.Max()) return false;
+            return true;
+        }
+    }
+}
---
+++ KataUnitTest/NextBiggerNumberTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kata;
+using NUnit.Framework;
+
+namespace KataUnitTest
+{
+    [TestFixture]
+    class NextBiggerNumberTest
+    {
+        [Test]
+        [TestCase(-1, 111)]
+        [TestCase(-1, 531)]
+        [TestCase(21, 12)]
+        [TestCase(531, 513)]
+        [TestCase(2071, 2017)]
+        [TestCase(441, 414)]
+        [TestCase(414, 144)]
+        public void BasicTests(long expected, long inputString)
+        {
+            var kata = new NextBiggerNumber();
+            long actual = kata.NextBigger(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
