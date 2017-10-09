439de5d4cc18e3f2594a5412a7fe2d7d6d63fbf6
CharlieHuang <notwist123@hotmail.com>
2017-10-09 22:38:37 +0800

    Add LeetCodeContainsDuplicateII

---
+++ Kata/LeetCodeContainsDuplicateII.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class LeetCodeContainsDuplicateII
+    {
+        public bool ContainsNearbyDuplicate(int[] inputArray, int absoluteDiff)
+        {
+            var duplicateIndex = inputArray.Select((x, i) => new {x, i}).GroupBy(g => g.x).Where(w => w.Count() > 1);
+
+            foreach (var g in duplicateIndex)
+            {
+                var groupArr = g.Select(x => x.i).ToArray();
+                for (var i = 0; i < groupArr.Length-1; i++)
+                {
+                    for (var j = i + 1; j < groupArr.Length; j++)
+                    {
+                        if (groupArr[j] - groupArr[i] <= absoluteDiff) return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ KataUnitTest/LeetCodeContainsDuplicateIITeset.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Kata;
+
+namespace KataUnitTest
+{
+    [TestFixture]
+    class LeetCodeContainsDuplicateIITeset
+    {
+        [Test]
+        [TestCase(new int[] { }, 0, false)]
+        [TestCase(new int[] { 1, 1 }, 1, true)]
+        [TestCase(new int[] { 99, 99 }, 2, true)]
+        [TestCase(new int[] { 1, 0, 1, 1 }, 1, true)]
+        public void BasicTests(int[] inputArray, int absoluteDiff, bool excepted)
+        {
+            var kata = new LeetCodeContainsDuplicateII();
+
+            var actual = kata.ContainsNearbyDuplicate(inputArray, absoluteDiff);
+
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
