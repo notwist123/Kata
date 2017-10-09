7408ab93a71793c9fcc76ec05eeead1221e7de7e
CharlieHuang <notwist123@hotmail.com>
2017-10-10 01:09:02 +0800

    Add LeetCodeContainsDuplicateIII

---
+++ Kata/LeetCodeContainsDuplicateIII.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class LeetCodeContainsDuplicateIII
+    {
+        public bool ContainsNearbyAlmostDuplicate(int[] inputArray, int absoluteDiffIndex, int absoluteDiffNumber)
+        {
+            if (absoluteDiffNumber != 0)
+            {
+                var duplicateIndex = inputArray.Select((x, i) => new {x, i}).OrderBy(l => l.x).ToArray();
+                var duplicateIndexCopy = duplicateIndex;
+
+                for (int i = 0; i < duplicateIndex.Length; i++)
+                {
+                    var maxNumber = duplicateIndexCopy[0].x + absoluteDiffNumber;
+                    var groupArr = duplicateIndexCopy.TakeWhile(x => x.x <= maxNumber).Select(s => s.i).ToArray();
+                    if (IsAbsoluteDiff(absoluteDiffIndex, groupArr)) return true;
+                    duplicateIndexCopy = duplicateIndexCopy.Skip(1).ToArray();
+                }
+                return false;
+            }
+            var duplicateIndex2 = inputArray.Select((x, i) => new { x, i }).GroupBy(g => g.x).Where(w => w.Count() > 1);
+
+            foreach (var g in duplicateIndex2)
+            {
+                var groupArr = g.Select(x => x.i).ToArray();
+                if (IsAbsoluteDiff(absoluteDiffIndex, groupArr)) return true;
+            }
+            return false;
+        }
+
+        private static bool IsAbsoluteDiff(int absoluteDiffIndex, int[] groupArr)
+        {
+            for (var i = 0; i < groupArr.Length - 1; i++)
+            {
+                for (var j = i + 1; j < groupArr.Length; j++)
+                {
+                    if (Math.Abs(groupArr[i] - groupArr[j]) <= absoluteDiffIndex) return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ KataUnitTest/LeetCodeContainsDuplicateIIITeset.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Kata;
+
+namespace KataUnitTest
+{
+    [TestFixture]
+    class LeetCodeContainsDuplicateIIITeset
+    {
+        [Test]
+        [TestCase(new int[] { -1, -1 }, 1, 0, true)]
+        [TestCase(new int[] { -3, 3 }, 2, 4, false)]
+        [TestCase(new int[] { 4, 2 }, 2, 1, false)]
+        [TestCase(new int[] { 2, 0, -2, 2 }, 2, 1, false)]
+        [TestCase(new int[] { 7, 2, 8 }, 2, 1, true)]
+        [TestCase(new int[] { 3, 6, 0, 4 }, 2, 2, true)]
+        public void BasicTests(int[] inputArray, int absoluteDiffIndex, int absoluteDiffNumber, bool excepted)
+        {
+            var kata = new LeetCodeContainsDuplicateIII();
+
+            var actual = kata.ContainsNearbyAlmostDuplicate(inputArray, absoluteDiffIndex, absoluteDiffNumber);
+
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
