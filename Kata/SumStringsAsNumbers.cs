50e07c955a0a26053f3b99190790de83fa4b6f20
CharlieHuang <notwist123@hotmail.com>
2017-10-14 21:50:24 +0800

    add SumStringsAsNumbers

---
+++ Kata/SumStringsAsNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace Kata
+{
+    public class SumStringsAsNumbers
+    {
+        private List<char> numberList;
+        private string numberA;
+        private string numberB;
+        private int carryNum;
+        public string sumStrings(string a, string b)
+        {
+            numberList = new List<char>();
+            carryNum = 0;
+            numberA = a;
+            numberB = b;
+
+            sumNumber(numberA.Length - 1, numberB.Length - 1);
+            numberList.Insert(0, carryNum.ToString()[0]);
+
+            return new string(numberList.ToArray()).TrimStart('0');
+        }
+
+        private void sumNumber(int i, int j)
+        {
+            if (i > -1 || j > -1)
+            {
+                int an = 0, bn = 0;
+                if (i >= 0) Int32.TryParse(numberA[i].ToString(), out an);
+                if (j >= 0) Int32.TryParse(numberB[j].ToString(), out bn);
+
+                string newNum = (an + bn + carryNum).ToString();
+
+                numberList.Insert(0, newNum[newNum.Length - 1]);
+                carryNum = newNum.Length > 1 ? 1 : 0;
+                sumNumber(i - 1, j - 1);
+            }
+        }
+    }
+}
---
+++ KataUnitTest/SumStringsAsNumbersTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using Kata;
+using NUnit.Framework;
+
+namespace KataUnitTest
+{
+    [TestFixture]
+    public class SumStringAsNumbersTest
+    {
+        [Test]
+        [TestCase("1079", new[] { "623", "456" })]
+        public void SampleTests(string expected, string[] inputString)
+        {
+            var Kata = new SumStringsAsNumbers();
+            var actual = Kata.sumStrings(inputString[0],inputString[1]);
+
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
