8df77cb93a7572abef40365cca0bd59850a6df62
CharlieHuang <notwist123@hotmail.com>
2017-10-22 06:40:12 +0800

    Add TennisScoring

---
+++ Kata/TennisScoring.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class TennisScoring
+    {
+        public string getScoringResult(int player1Win, int player2Win)
+        {
+            List<string> scoringList = new List<string> {"Love","Fifteen","Thirty","Forty"};
+            string player1Result;
+            string player2Result;
+
+            if ((player1Win == player2Win) && (player1Win >= 3)) return "Deuce.";
+            if (player1Win > 3 || player2Win > 3)
+            {
+                if (player1Win - player2Win >= 2) return "Player1 Win.";
+                if (player2Win - player1Win >= 2) return "Player2 Win.";
+                if (player1Win - player2Win == 1) return "Player1 Advantage.";
+                if (player2Win - player1Win == 1) return "Player2 Advantage.";
+            }
+            player1Result = scoringList[player1Win];
+            player2Result = scoringList[player2Win];
+
+            return player1Result + " - " + player2Result;
+        }
+    }
+}
---
+++ KataUnitTest/TennisScoringTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using Kata;
+
+namespace KataUnitTest
+{
+    [TestFixture]
+    public class TennisScoringTest
+    {
+        [Test]
+        [TestCase(0, 0, "Love - Love")]
+        [TestCase(1, 0, "Fifteen - Love")]
+        [TestCase(2, 0, "Thirty - Love")]
+        [TestCase(3, 0, "Forty - Love")]
+        [TestCase(3, 1, "Forty - Fifteen")]
+        [TestCase(3, 3, "Deuce.")]
+        [TestCase(4, 4, "Deuce.")]
+        [TestCase(4, 0, "Player1 Win.")]
+        [TestCase(1, 4, "Player2 Win.")]
+        [TestCase(8, 6, "Player1 Win.")]
+        [TestCase(6, 8, "Player2 Win.")]
+        [TestCase(4, 5, "Player2 Advantage.")]
+        [TestCase(5, 4, "Player1 Advantage.")]
+        public void BasicTests(int player1Win, int player2Win, string excepted)
+        {
+            var kata = new TennisScoring();
+
+            var actual = kata.getScoringResult(player1Win, player2Win);
+
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
