5fdc192b7986ad96e8f1ac1f7b97af4c8c4e2876
Cash Wu <cash@cashwu.com>
2017-11-13 09:02:20 -0600

    Update PokerHandsTest.cs
    refactor test case

--- KataUnitTest/PokerHandsTest.cs
+++ KataUnitTest/PokerHandsTest.cs
@@ -7,8 +7,8 @@ namespace KataUnitTest
     public class PokerHandsTest
     {
         [Test]
-        [TestCase(new[] { "2H", "3D", "5S", "9C", "KD"}, new[] { "2C", "3H", "4S", "8C", "AH"},"White wins. - with highcard: Ace")]
-        [TestCase(new[] { "2H", "3D", "5S", "9C", "KD"}, new[] { "2C", "3H", "4S", "8C", "KH"},"Tie.")]
+        [TestCase(new[] { "2H", "3D", "5S", "9C", "KD"}, new[] { "2H", "3D", "5S", "9C", "AH"},"White wins. - with highcard: Ace")]
+        [TestCase(new[] { "2H", "3D", "5S", "9C", "KD"}, new[] { "2H", "3D", "5S", "9C", "KD"},"Tie.")]
 
         public void SampleTests(string[] black, string[] white, string expected)
         {
